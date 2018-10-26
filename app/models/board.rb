a5792a940a7bf50300e9d9015e13b7b24afe1dbb
Paritosh Singh <i@paritoshsingh.com>
2018-10-26 20:37:58 +0530

    Updated functionality added

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,9 +3,9 @@ class Board
 
 
   def self.initialize(size = 20)
-    (1..size).each do |row|
+    (0..(size - 1)).each do |row|
         @@board[row] = []
-      (1..size).each do |column|
+      (0..(size - 1)).each do |column|
         @@board[row] << Cell.new(row, column, Pallete::DEFAULT_COLOR)
       end
     end
@@ -18,7 +18,7 @@ class Board
   end
 
 
-  def self.set_value row, column, color, updated_by        
+  def self.set_value row, column, color, updated_by    
     cell = (@@board[row] || [])[column]
     raise "Invalid cell_value" if cell.blank?
     cell.update_color color, updated_by
